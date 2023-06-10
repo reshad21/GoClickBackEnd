c0ec7fef4ee2a4ebff27d8111e6bace71dc79908
reshad21 <miakala7@gmail.com>
2023-06-10 12:34:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+})
+
+
+// Cc7Ebl2uR91MQ88n
+// goclick
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gplljg9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db("goclick").collection("products");
+
+
+        app.post('/product', async (req, res) => {
+            const data = req.body;
+            const result = await productCollection.insertOne(data);
+            res.send(result);
+        });
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const result = await productCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.put('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const formData = req.body;
+
+            const query = { orderid: id };
+            // console.log(query);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    ...formData,
+                    price: formData.price,
+                },
+            };
+            const result = await productCollection.updateOne(query, updateDoc, options);
+            // res.send(query);
+            res.send(result);
+        });
+
+        app.get('/product/search', async (req, res) => {
+            const searchTerm = req.query.term;
+            console.log(searchTerm);
+            const searchData = await productCollection.find({ name: { $regex: searchTerm, $options: 'i' } });
+            // res.json(searchData);
+            // const data = res.json(searchData);
+            res.send(searchData);
+
+        })
+
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`Listening to the port ${port}`);
+})
